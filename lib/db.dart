63c95509ff802d9ad13bb3f874396ecad05e1755
asl <promotionbd321@gmail.com>
2026-01-18 16:17:51 +0600

    connected to mongodb

---
+++ lib/db.dart
@@ -0,0 +1,30 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
+class DB {
+  static late Db db;
+  static late DbCollection messagesCollection;
+
+  static Future<void> init() async {
+    // MongoDB URI: যদি local MongoDB থাকে
+    db = Db('mongodb://localhost:27017/chat_app');
+
+    await db.open();
+    print('✅ Connected to MongoDB');
+
+    // Collection
+    messagesCollection = db.collection('messages');
+  }
+
+  static Future<void> saveMessage(Map<String, dynamic> msg) async {
+    await messagesCollection.insertOne(msg);
+  }
+
+  static Future<List<Map<String, dynamic>>> getMessagesForUser(String userId) async {
+    return await messagesCollection.find({
+      r'$or': [
+        {'from': userId},
+        {'to': userId},
+      ]
+    }).toList();
+  }
+}
